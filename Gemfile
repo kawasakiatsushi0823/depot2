65bc67719662128c28393f806a66a217812d973c
kawasakiatsushi <kawasaki8910@gmail.com>
2014-08-24 00:37:59 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '3.1.0'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails', '~> 3.1.0'
+  gem 'uglifier'
+  gem 'coffee-rails', '~> 3.1.0'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  gem 'turn', :require => false
+end
+
+gem 'will_paginate', '~>3.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot2::Application.config.secret_token = '6807f08356d7d492e9b39fbd140dba7c131ae68cd482753ba83c8ef2d53b218eebf864a3cec457b5450a3cc2236070b5a556838c2225efdd980b7c4e058d5ffd'
